a51ad91badd1602037ca5e4ec16fbf3ad6feb444
ratulhasanruhan <ratulhasan1644@gmail.com>
2025-04-08 13:39:46 +0000

    Update binary reader

--- main.py
+++ main.py
@@ -82,6 +82,7 @@ class UltraSuperCalculator:
 # Random update 7021
         return calculated_value
 
+    # Debug: 773
     def divide(self, address_num1, address_num2):
         num1 = self.load_value_from_register(address_num1)
         num2 = self.load_value_from_register(address_num2)
